15ffb3205590a395d249dc2815dd6a6c319a2732
vicki-5 <vpavloski@gmail.com>
2020-03-30 23:11:04 -0700

    Added default assignments to player class

--- server/player.js
+++ server/player.js
@@ -1,8 +1,33 @@
+const defaultValues = require('./constants');
+
 class Player {
-  constructor(socket, name) {
-    this.name = name
-    this.socket = socket;
-    this.lobby = undefined;
-    this.person = undefined;
+  constructor(playername, character) {
+    this.playername = playername;
+    let id;
+    let location;
+    let key;
+    let data = JSON.parse(defaultValues);
+
+
+    switch(character) {
+      case "MissScarlet": key = 'scarlet'; break;
+      case "ProfessorPlum": key = 'plum'; break;
+      case "ColonelMustard": key = 'mustard'; break;
+      case "MrsPeacock": key = 'peacock'; break;
+      case "MrGreen": key = 'green'; break;
+      case "MrsWhite": key = 'white'; break;
+    }
+
+    for (let j in data) {
+      if (j === key) {
+        for (let x in data[key]) {
+          this.id = data[key][id];
+          this.location = data[key][location];
+          this.character = data[key][name];
+        }
+      }
+    }
+
   }
 }
+
