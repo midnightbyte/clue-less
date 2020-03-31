b53e8685ed1eea0ea9c4a821ced427a9137ccd1a
jpmcelderry <john.p.mcelderry@gmail.com>
2020-03-30 23:45:51 -0400

    made changes to gameState

--- server/gameState.js
+++ server/gameState.js
@@ -1,8 +1,28 @@
 class GameState {
   constructor(players) {
-
+	  this.playersList = players;
   }
 
+  this.playersList = [];
+  this.turnQueue = [];
+  this.turnPhases = [];
+  
+  checkActivePlayer(){
+	  return this.turnQueue[0];
+  }
+  
+  checkTurnPhase(){
+	  return this.turnPhases[0];
+  }
+  
+  shiftTurnPhase(){
+	  this.turnPhases.push(this.turnPhases.shift());
+  }
+  
+  changeActivePlayer(){
+	  this.turnQueue.push(this.turnQueue.shift());
+  }
+  
   update() {
 
   }
