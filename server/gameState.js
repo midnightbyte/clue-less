da238a007e170fbee11d1dfbb5b942aed69327a2
vicki-5 <vpavloski@gmail.com>
2020-03-30 23:13:42 -0700

    commented errors from gamestate file

--- server/gameState.js
+++ server/gameState.js
@@ -3,26 +3,26 @@ class GameState {
 	  this.playersList = players;
   }
 
-  this.playersList = [];
-  this.turnQueue = [];
-  this.turnPhases = [];
-  
+//  this.playersList = [];
+//  this.turnQueue = [];
+//  this.turnPhases = [];
+
   checkActivePlayer(){
 	  return this.turnQueue[0];
   }
-  
+
   checkTurnPhase(){
 	  return this.turnPhases[0];
   }
-  
+
   shiftTurnPhase(){
 	  this.turnPhases.push(this.turnPhases.shift());
   }
-  
+
   changeActivePlayer(){
 	  this.turnQueue.push(this.turnQueue.shift());
   }
-  
+
   update() {
 
   }
