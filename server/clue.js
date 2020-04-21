c9ac511ee12691c81f8e8437c31c5bf84d453577
Gage Benne <gagedbenne@gmail.com>
2020-04-21 14:27:00 -0400

    Revise clue

--- server/clue.js
+++ server/clue.js
@@ -1,51 +1,32 @@
-const CONSTANTS = require('./constants.json')
-const PERSONS = CONSTANTS["persons"]
-const SPACES = {...CONSTANTS["rooms"],...CONSTANTS["hallways"]}
-
 class Clue {
-  constructor(id) {
+  constructor(id, location) {
     this.id = id;
+    location = location;
   }
 }
 
 class Weapon extends Clue {
-  constructor(weapon) {
+  constructor(weapon, location) {
     super(weapon);
   }
 }
 
 class Room extends Clue {
-  constructor(room, isHallway) {
-    super(room);
-    this.paths = SPACES[room]["paths"]
+  constructor(room, location) {
+    super(room, location);
   }
 }
 
-class Hallway {
-
-}
-
 class Person extends Clue {
-  constructor(color, name=undefined) {
-    super(color);
+  constructor(color, location, name) {
+    super(color, location);
     this.color = color
-    if (username) {
-      this.name = name;
-    }
-    this.username = PERSONS[color]["name"];
-    this.location = PERSONS[color]["location"];
-    this.hasLost = false;
+    this.name = name
 
     this.clues = [];
     this.seen = [];
     this.checklist = undefined;
   }
-
-  move(space) {
-    player.location = SPACES[space]
-  }
 }
-
-Person.connect(socket)
 
 module.exports = Clue;
