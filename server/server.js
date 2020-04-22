b208572efbda884cb267026311fd5a79f61e4aff
Gage Benne <gagedbenne@gmail.com>
2020-04-21 20:56:24 -0400

    Emit when player connected

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ module.exports = function(io){
   let playerService = new PlayerService();
   io.sockets.on('connection', function(socket) {
     Player.connect(socket, playerService);
+    io.emit('playerConnected');
 
     socket.on('disconnect', function() {
       Player.disconnect(socket, playerService);
+      io.emit('playerDisconnected');
     });
   });
 }
