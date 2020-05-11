286b57dc6e1e602d971a696085b77370c3f8df82
vicki-5 <vpavloski@gmail.com>
2020-05-10 20:40:45 -0700

    Renamed space variables and added comments.

--- server/space.js
+++ server/space.js
@@ -1,8 +1,8 @@
 module.exports = {
-  Space: function(name, isRoom, characters) {
-    this.name = name;
-    this.isRoom = isRoom;
-    this.characters = characters;
-    this.nextSpaces = [];
+  Space: function(name, isRoom, whoHere) {
+    this.name = name;       // space name
+    this.isRoom = isRoom;   // if not a room, it's a hallway
+    this.whoHere = whoHere; // who is on this space
+    this.nextSpaces = [];   // list of next door spaces
   }
 };
